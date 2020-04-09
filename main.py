ab11d02a4beb7838081ddf2c22f813e132e5d95c
Dustin <dustinsurwill@gmail.com>
2020-04-08 22:42:07 -0700

    + options menu % try multiprocess (not currently working)

--- main.py
+++ main.py
@@ -1,16 +1,91 @@
 from _csv import writer
+from functools import partial
+from multiprocessing import cpu_count, Pool
 from os.path import basename
+from threading import Thread
+from typing import List
 
 import docx2txt
 from PyPDF2 import PdfFileReader
-from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QApplication, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, QDialog, QGridLayout, QStyleFactory, QListWidget, QFileDialog, QTableWidget, QTableWidgetItem, QHeaderView
+from PyQt5.QtGui import QColor, QPalette
+from PyQt5.QtWidgets import QApplication, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QStyleFactory, QListWidget, QFileDialog, QTableWidget, QTableWidgetItem, QHeaderView, QMainWindow, QWidget, QDialog, QColorDialog
 
 
-class FileScanner(QDialog):
-    def __init__(self, parent=None) -> None:
+def scan_files_process(keywords: List[str], file: str) -> List[bool]:
+    if file[-4:] == '.pdf':
+        with open(file, 'rb') as process_file:
+            pdf = PdfFileReader(process_file)
+            process_file_text = '\n'.join(pdf.getPage(i).extractText().upper() for i in range(pdf.getNumPages()))
+    elif file[-4:] == 'docx':
+        process_file_text = docx2txt.process(file).upper()
+    else:
+        with open(file, 'rt') as process_file:
+            process_file_text = process_file.read()
+    r = [k in process_file_text for k in keywords]
+    return r
+
+
+class Options(QDialog):
+    def __init__(self, parent):
         super().__init__(parent)
+        self.found_color = QColor('green')
+        self.missing_color = QColor('red')
+        self.found_text = 'true'
+        self.missing_text = 'false'
         main_layout = QGridLayout()
+        self.found_color_button = QPushButton('Found Color Selector')
+        self.found_color_button.clicked.connect(self.select_found_color)
+        self.found_color_button.setAutoFillBackground(True)
+        self.found_color_button.setFlat(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Button, self.found_color)
+        self.found_color_button.setPalette(palette)
+        main_layout.addWidget(self.found_color_button, 0, 0, 1, 1)
+        self.missing_color_button = QPushButton('Missing Color Selector')
+        self.missing_color_button.clicked.connect(self.select_missing_color)
+        self.missing_color_button.setAutoFillBackground(True)
+        self.missing_color_button.setFlat(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Button, self.missing_color)
+        self.missing_color_button.setPalette(palette)
+        main_layout.addWidget(self.missing_color_button, 1, 0, 1, 1)
+        self.found_text_input = QLineEdit(self.found_text)
+        self.found_text_input.textEdited.connect(self.edit_found_text)
+        main_layout.addWidget(self.found_text_input, 0, 1, 1, 1)
+        self.missing_text_input = QLineEdit(self.missing_text)
+        self.missing_text_input.textEdited.connect(self.edit_missing_text)
+        main_layout.addWidget(self.missing_text_input, 1, 1, 1, 1)
+        self.setLayout(main_layout)
+        self.setWindowTitle('Options')
+
+    def edit_missing_text(self):
+        self.missing_text = self.missing_text_input.text()
+
+    def edit_found_text(self):
+        self.found_text = self.found_text_input.text()
+
+    def select_found_color(self):
+        color = QColorDialog.getColor(self.found_color, self, 'Found Color')
+        if color.isValid():
+            self.found_color = color
+            palette = QPalette()
+            palette.setColor(QPalette.Button, self.found_color)
+            self.found_color_button.setPalette(palette)
+
+    def select_missing_color(self):
+        color = QColorDialog.getColor(self.missing_color, self, 'Missing Color')
+        if color.isValid():
+            self.missing_color = color
+            palette = QPalette()
+            palette.setColor(QPalette.Button, self.missing_color)
+            self.missing_color_button.setPalette(palette)
+
+
+class FileScanner(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        center = QWidget()
+        main_layout = QGridLayout(center)
         main_layout.addLayout(self.create_keyword_area(), 0, 0)
         main_layout.setColumnMinimumWidth(0, 350)
         self.file_headers = ['files']
@@ -18,11 +93,11 @@ class FileScanner(QDialog):
         main_layout.setColumnMinimumWidth(1, 650)
         main_layout.setColumnStretch(1, 1)
         main_layout.setRowMinimumHeight(0, 500)
-        self.setLayout(main_layout)
+        self.setCentralWidget(center)
         self.setWindowTitle('File Scanner')
         QApplication.setStyle(QStyleFactory.create('windowsvista'))
         self.file_names = []
-        # self.showMaximized()
+        self.options = Options(self)
         self.show()
 
     def create_files_area(self):
@@ -49,6 +124,9 @@ class FileScanner(QDialog):
         load_keywords = QPushButton('Load Keywords')
         load_keywords.clicked.connect(self.load_keywords_clicked)
         buttons.addWidget(load_keywords)
+        options = QPushButton('Options')
+        options.clicked.connect(lambda: self.options.show())
+        buttons.addWidget(options)
         self.scan_files = QPushButton('Scan Files')
         self.save_keywords.setDisabled(True)
         self.scan_files.clicked.connect(self.scan_files_clicked)
@@ -79,21 +157,14 @@ class FileScanner(QDialog):
             self.update_file_headers()
 
     def scan_files_clicked(self):
-        for row in range(self.files.rowCount()):
-            file = self.file_names[row]
-            if file[-4:] == '.pdf':
-                with open(file, 'rb') as process_file:
-                    pdf = PdfFileReader(process_file)
-                    process_file_text = '\n'.join(pdf.getPage(i).extractText().upper() for i in range(pdf.getNumPages()))
-            elif file[-4:] == 'docx':
-                process_file_text = docx2txt.process(file).upper()
-            else:
-                with open(file, 'rt') as process_file:
-                    process_file_text = process_file.read()
-            for i in range(self.keyword_list.count()):
-                has = self.keyword_list.item(i).text().upper() in process_file_text
-                widget = QTableWidgetItem(str(has).upper())
-                widget.setBackground(QColor('green' if has else 'red'))
+        # with Pool(min(cpu_count(), len(self.file_names))) as pool:
+        f = partial(scan_files_process, [self.keyword_list.item(i).text().upper() for i in range(self.keyword_list.count())])
+        r = [f(file) for file in self.file_names]
+        # r = pool.map(f, self.file_names)
+        for row, has_list in enumerate(r):
+            for i, has in enumerate(has_list):
+                widget = QTableWidgetItem(self.options.found_text if has else self.options.missing_text)
+                widget.setBackground(self.options.found_color if has else self.options.missing_color)
                 self.files.setItem(row, i + 1, widget)
         self.export_results.setDisabled(False)
 
